0bf02b6fbb0ef377106c052a3ae23fb9b36c97eb
EricLam414 <71828524+EricLam414@users.noreply.github.com>
2021-01-22 17:40:46 -0500

    starting code

---
+++ 00-SuperArray/SuperArray.java
@@ -0,0 +1,32 @@
+public class SuperArray{
+  private int[] array;
+  // constructor
+  public SuperArray(int s){
+    this.array = new int[s];
+  }
+  public SuperArray(){
+    this.array = new int[10];
+  }
+  public void add(int val){
+
+  }
+  public void add(int index, int val){
+
+  }
+  public void grow(int n){
+    
+  }
+  //sets value at a specific index
+  public void set(int i, int val){
+    this.array[i] = val;
+  }
+  public boolean isEmpty(){
+
+  }
+  public void remove(int index){
+
+  }
+  public String toString(){
+
+  }
+}
