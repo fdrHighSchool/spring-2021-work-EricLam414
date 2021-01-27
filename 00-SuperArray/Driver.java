f2abfdadef98ca9436055295ee823c0673b58a52
EricLam414 <71828524+EricLam414@users.noreply.github.com>
2021-01-27 09:04:49 -0500

    added remove()

--- 00-SuperArray/Driver.java
+++ 00-SuperArray/Driver.java
@@ -1,19 +1,16 @@
 public class Driver{
   public static void main(String[] args) {
     SuperArray array1 = new SuperArray();
-    array1.add(10);
-    array1.add(20);
-    array1.add(30);
-    array1.add(40);
-    array1.add(50);
-    array1.add(60);
-    array1.add(70);
-    array1.add(80);
-    array1.add(90);
-    array1.add(100);
-    array1.add(110);
+    System.out.println(array1.isEmpty());
+    for(int i = 10; i <= 110; i += 10){
+      array1.add(i);
+    }
     System.out.println(array1.toString());
+    System.out.println(array1.isEmpty());
+    array1.set(4,5);
     array1.grow(1);
+    System.out.println(array1.toString());
+    array1.remove(2);
     System.out.println(array1.toString());
   } //end main
 } //end class
--- 00-SuperArray/SuperArray.java
+++ 00-SuperArray/SuperArray.java
@@ -2,15 +2,18 @@ import java.util.*;
 
 public class SuperArray{
   private int[] array;
+
   // constructor
   public SuperArray(int s){
     this.array = new int[s];
     System.out.println(this.array.length);
   }
+
   public SuperArray(){
     this.array = new int[10];
     System.out.println(this.array.length);
   }
+
   public void add(int val){
     for(int i = 0; i < this.array.length; i++){
       if (array[i] == 0){
@@ -26,31 +29,49 @@ public class SuperArray{
   public void add(int index, int val){
 
   }
+
   public void grow(int n){
     int[] tempArray = new int[this.array.length];
+
     for(int i = 0; i < this.array.length; i++){
        tempArray[i] = this.array[i];
     }
+
     this.array = new int[n + this.array.length];
+
     for(int i = 0; i < tempArray.length; i++){
       this.array[i] = tempArray[i];
     }
+
   }
   //sets value at a specific index
   public void set(int i, int val){
     this.array[i] = val;
   }
+
   public boolean isEmpty(){
     for(int i = 0; i < this.array.length; i++){
       if (array[i] != 0){
-        return true;
+        return false;
       }
     }
-    return false;
+    return true;
   }
-  public void remove(int index){
 
+  public void remove(int index){
+    int[] temp = new int[this.array.length - 1];
+
+    for(int i = 0; i < temp.length; i++){
+      if(i >= index){
+        temp[i] = this.array[i + 1];
+      }
+      else{
+        temp[i] = this.array[i];
+      }
+    }
+    this.array = temp;
   }
+
   public String toString(){
     String str = "{";
     for (int i = 0; i < this.array.length; i++){
