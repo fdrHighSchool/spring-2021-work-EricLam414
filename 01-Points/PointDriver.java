dbaac95801ed347acd7a43f6386f583179428536
EricLam414 <71828524+EricLam414@users.noreply.github.com>
2021-03-02 08:17:06 -0500

    finished points

---
+++ 01-Points/PointDriver.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+
+public class PointDriver {
+    public static void main(String[] args) {
+        ArrayList<Point> points = new ArrayList<>();
+
+        points.add(new Point());
+        System.out.println(points.get(0).toString());
+        points.add(new Point(3, 4));
+        points.add(new Point(6, 8));
+
+        System.out.println(points.get(0).slope(points.get(1)));
+        System.out.println(points.get(0).midpoint(points.get(1)));
+        System.out.println(points.get(0).distance(points.get(1)));
+
+        System.out.println(points.get(0).isCollinear(points.get(1), points.get(2)));
+
+    }
+}
